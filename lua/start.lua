415e970afdb2ef453100f49e0dc4404f495d03f0
borko17 <41315080+borko17@users.noreply.github.com>
2025-10-09 09:32:38 +0200

    Update start.lua

--- lua/start.lua
+++ lua/start.lua
@@ -2,22 +2,22 @@ local url = "https://raw.githubusercontent.com/borko17/pravoslavlje/refs/heads/m
 
 local handle = io.popen("curl -s -L " .. url)
 if not handle then
-  print("Није могуће покренути curl.")
-  return
+    print("Није могуће покренути curl.")
+    return
 end
 
 local kod = handle:read("*a")
 handle:close()
 
 if not kod or kod == "" then
-  print("Провјерите интернет конекцију.")
-  return
+    print("Провјерите интернет конекцију.")
+    return
 end
 
 local fn, err = load(kod)
 if not fn then
-  print("Грешка при учитавању скрипте: " .. tostring(err))
-  return
+    print("Грешка при учитавању скрипте: " .. tostring(err))
+    return
 end
 
 fn()
